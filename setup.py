92c0c6018e49be67c061a3723f54379cb0abe637
eightnoteight <mr.eightnoteight@gmail.com>
2015-12-26 12:04:26 +0530

    lets formalize a bit

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
-      version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
-      url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
-     )
+setup(
+    name='mr_box',
+    version='0.1',
+    description='hosts some of the simple scripts that i have made over the time.',
+    author='Srinivas Devaki',
+    author_email='mr.eightnoteight@gmail.com',
+    url='http://www.python.org/sigs/distutils-sig/',
+)
